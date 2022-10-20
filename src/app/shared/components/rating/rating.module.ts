5dcb8c273d7810af280932fc1cce43298a68c6ca
Nicolas Naso <nicolasna@arionkoder.com>
2022-10-20 16:21:44 -0300

    feat: integrate fortawesome and add it to rating component

--- src/app/shared/components/rating/rating.module.ts
+++ src/app/shared/components/rating/rating.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FortAwesomeModule } from '@shared/fort-awesome.module';
 import { RatingComponent } from './rating.component';
 
-const MODULES: any[] = [CommonModule, FormsModule, ReactiveFormsModule];
+const MODULES: any[] = [CommonModule, FormsModule, ReactiveFormsModule, FortAwesomeModule];
 const COMPONENTS: any[] = [RatingComponent];
 
 @NgModule({
